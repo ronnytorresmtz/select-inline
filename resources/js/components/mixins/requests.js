7e7d8f7fa3339e5ff019fb9f7365962d9b555344
Rolando Torres <ronnytorresmtz@gmail.com>
2019-06-08 11:33:34 -0500

    handle fields that are required avoid 422

--- resources/js/components/mixins/requests.js
+++ resources/js/components/mixins/requests.js
@@ -2,12 +2,18 @@ export default {
     methods: {
 
         updateResourceId(value) {
-
+            const fields = this.$parent.resource.fields;
             let formData = new FormData();
+            Object.keys(fields).forEach((key)=> {
+                if (fields[key].attribute !== this.field.attribute){
+                    formData.append(fields[key].attribute, fields[key].value);
+                } else {
+                    formData.append(fields[key].attribute, value);
+                }
+            });
 
-            formData.append(this.field.attribute, value);
             formData.append('_method', 'PUT');
-            
+
             return Nova.request().post(`/nova-api/${this.resourceName}/${this.resourceId}`, formData)
                 .then(() => {
                     const option = this.getOptionLabel(this.field.options, value);
